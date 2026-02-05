ca66c6b565ad7d568594762891542ad418d6916e
Laptop Alexandre <alexandrecandidol5@gmail.com>
2026-02-05 17:26:40 -0300

    Slf4j to Log

--- example_21/src/main/java/com/eazybytes/eazyschool/service/ContactService.java
+++ example_21/src/main/java/com/eazybytes/eazyschool/service/ContactService.java
@@ -1,15 +1,14 @@
 package com.eazybytes.eazyschool.service;
 
-import com.eazybytes.eazyschool.controller.ContactController;
 import com.eazybytes.eazyschool.model.Contact;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 public class ContactService {
 
-    private static Logger log = LoggerFactory.getLogger(ContactController.class);
+    //private static Logger log = LoggerFactory.getLogger(ContactController.class);
 
     /*
     Save Contact Details into DB
