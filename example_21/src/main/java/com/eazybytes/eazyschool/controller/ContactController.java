9bf24c8f0b0a6bc94fb675e43df79030dfff4b81
Laptop Alexandre <alexandrecandidol5@gmail.com>
2026-02-05 17:29:06 -0300

    Slf4j to Log

--- example_21/src/main/java/com/eazybytes/eazyschool/controller/ContactController.java
+++ example_21/src/main/java/com/eazybytes/eazyschool/controller/ContactController.java
@@ -2,6 +2,7 @@ package com.eazybytes.eazyschool.controller;
 
 import com.eazybytes.eazyschool.model.Contact;
 import com.eazybytes.eazyschool.service.ContactService;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,10 +14,9 @@ import org.springframework.web.servlet.ModelAndView;
 
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
+@Slf4j
 @Controller
 public class ContactController {
-
-    private static Logger log = LoggerFactory.getLogger(ContactController.class);
 
     private final ContactService contactService;
 
