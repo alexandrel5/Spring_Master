0242696d14061dcf56105b2dfac0374bc54e1219
Laptop Alexandre <alexandrecandidol5@gmail.com>
2026-02-06 16:04:43 -0300

    Example22 with optional parameter

---
+++ example_22/src/main/java/com/eazybytes/eazyschool/ExampleApplication.java
@@ -0,0 +1,15 @@
+package com.eazybytes.eazyschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.webmvc.autoconfigure.error.ErrorMvcAutoConfiguration;
+
+//@SpringBootApplication(exclude = {ErrorMvcAutoConfiguration.class})
+@SpringBootApplication
+public class ExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleApplication.class, args);
+	}
+
+}
